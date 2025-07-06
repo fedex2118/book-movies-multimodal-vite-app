c91552c59560b05511dd3f9f58a44d17b69ef0ad
Federico Ziegler <105019871+fedex2118@users.noreply.github.com>
2025-07-06 17:41:45 +0200

    added comments to normalization functions

--- src/hooks/computePalmNormal.js
+++ src/hooks/computePalmNormal.js
@@ -1,12 +1,21 @@
 function computePalmNormal(lm) {
-    const p0 = lm[0], p1 = lm[5], p2 = lm[17];
+    // we want to find the perpendicular direction with respect to the hand palm
+    // this gives us the orientation of the hand
+    const p0 = lm[0], p1 = lm[5], p2 = lm[17]; // wrist, base of index finger, base of pinky finger... form a triangle on the palm plane
+    // now we create two vectors in the palm plane
+    // v1 = from wrist to base of index
+    // v2 = from base of index to base of pinky
     const v1 = { x: p1.x - p0.x, y: p1.y - p0.y, z: p1.z - p0.z };
     const v2 = { x: p2.x - p1.x, y: p2.y - p1.y, z: p2.z - p1.z };
     // cross product v1 × v2
+    // this is usefull to have a vector perpendicular to the palm plane
     const nx = v1.y * v2.z - v1.z * v2.y;
     const ny = v1.z * v2.x - v1.x * v2.z;
     const nz = v1.x * v2.y - v1.y * v2.x;
+    // we now can compute the length of the 3d vector with the L2 norm, fallback is 1 to avoid NaN or infinity when dividing by 0 
     const norm = Math.hypot(nx, ny, nz) || 1;
+    // this normalization factor is used to normalized the perpendicular vector so that it's length is always 1
+    // result = we got a unit vector that points at the same direction as the original vector
     return [ nx / norm, ny / norm, nz / norm ];
 }
 
--- src/hooks/normalizeLandmarks.js
+++ src/hooks/normalizeLandmarks.js
@@ -1,14 +1,18 @@
 function normalizeLandmarks(lm) {
-    const [wx, wy, wz] = [lm[0].x, lm[0].y, lm[0].z];
+    const [wx, wy, wz] = [lm[0].x, lm[0].y, lm[0].z]; // we take the wrist position as the origin of our hand
     const centered = lm.map(pt => ({
         x: pt.x - wx,
         y: pt.y - wy,
         z: pt.z - wz
-    }));
+    })); // we center each hand point around the wrist position
+    // this means that the origin (wrist) is now (0, 0, 0), all points are translated
     let maxDist = 0;
     centered.forEach(pt => {
-        maxDist = Math.max(maxDist, Math.hypot(pt.x, pt.y, pt.z));
+        maxDist = Math.max(maxDist, Math.hypot(pt.x, pt.y, pt.z)); // we get the maximum distance from the origin 
+        // max hypot is the L2 norm computed for of each point
     });
+    // we now scale each point by the maximum distance, so the largest distance becomes 1
+    // we flatten the array into a single number array. [x1, y1, z1, x2, y2, z2, ...]
     return centered.flatMap(pt => [pt.x / maxDist, pt.y / maxDist, pt.z / maxDist]);
 }
 
